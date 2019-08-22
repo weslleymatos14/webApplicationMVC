f9eeb8fde9fdfdc68eab2209a63150357d9613be
weslley matos <weslleymatos.github@gmail.com>
2019-08-22 12:59:09 -0300

    Segunda Migration 'OtherEntities'

--- WebApplicationMVC/Data/WebApplicationMVCContext.cs
+++ WebApplicationMVC/Data/WebApplicationMVCContext.cs
@@ -13,6 +13,8 @@ namespace WebApplicationMVC.Models
         {
         }
 
-        public DbSet<WebApplicationMVC.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
     }
 }
---
+++ WebApplicationMVC/Models/Seller.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebApplicationMVC.Models
+{
+    public class Seller
+    {
+        public int Id { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller()
+        {
+        }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr)
+        {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr)
+        {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final)
+        {
+            return Sales.Where(x => x.Date >= initial && x.Date <= final).Sum(x => x.Amount);
+        }
+    }
+}
