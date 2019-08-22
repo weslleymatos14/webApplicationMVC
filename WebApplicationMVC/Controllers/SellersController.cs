cde0def496a1606bf03c92a8ea27460975a6cf58
weslley matos <weslleymatos.github@gmail.com>
2019-08-22 16:21:38 -0300

    SellerFormViewModel

--- WebApplicationMVC/Controllers/SellersController.cs
+++ WebApplicationMVC/Controllers/SellersController.cs
@@ -1,20 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApplicationMVC.Models;
 using WebApplicationMVC.Services;
+using WebApplicationMVC.Models.ViewModels;
 
 namespace WebApplicationMVC.Controllers
 {
     public class SellersController : Controller
     {
         private readonly SellerService _sellerService;
+        private readonly DepartmentService _departmentService;
 
-        public SellersController(SellerService sellerService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
         {
             _sellerService = sellerService;
+            _departmentService = departmentService;
         }
 
         public IActionResult Index()
@@ -25,7 +27,9 @@ namespace WebApplicationMVC.Controllers
 
         public IActionResult Create()
         {
-            return View();
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
         }
 
         [HttpPost]
---
+++ WebApplicationMVC/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using WebApplicationMVC.Models;
+
+namespace WebApplicationMVC.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebApplicationMVCContext _context;
+
+        public DepartmentService(WebApplicationMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
