a09d8f978b7f0a3bf2eb88bfd5a163c0b69cbd7d
weslley matos <weslleymatos.github@gmail.com>
2019-08-22 16:46:44 -0300

    Eager Loading

--- WebApplicationMVC/Controllers/SellersController.cs
+++ WebApplicationMVC/Controllers/SellersController.cs
@@ -63,5 +63,21 @@ namespace WebApplicationMVC.Controllers
             _sellerService.remove(id);
             return RedirectToAction(nameof(Index));
         }
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellerService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
--- WebApplicationMVC/Services/SellerService.cs
+++ WebApplicationMVC/Services/SellerService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebApplicationMVC.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApplicationMVC.Services
 {
@@ -28,7 +29,7 @@ namespace WebApplicationMVC.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void remove(int id)
